97e16cf3f34bb9e7143c3b1b332e0dc7c5347607
Arquimedes <arquimed@protonmail.com>
2018-08-23 02:53:53 +0200

    React Boilerplate added + old files removed

--- ethereum/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var IBT = artifacts.require("IBT");
-
-
-module.exports = function(deployer) {
-  deployer.deploy(IBT); // if constructor expects arguments, add them here such as deployer.deply(IBT, argument1, argument 2)
-};
